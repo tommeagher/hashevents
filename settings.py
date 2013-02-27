5bc35d3c07c272b2085743c6c4b89e4179ea2f22
Tom Meagher <car+bit@tommeagher.com>
2013-02-27 05:26:15 -0600

    cleaned up template

--- settings.py
+++ settings.py
@@ -1,5 +1,4 @@
 #config
 HASHTAG = "#NICAR13"
 #here, put the tweetid of the earliest tweet you want to aggregate for the tables
-JSONID=306210377006469120
-#JSONID=304704581642907649
+JSONID=306634813970972672
